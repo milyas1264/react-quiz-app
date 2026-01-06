e057b54166553f07dafdd62ee44298713e0c3594
milyas1264 <milyasather42401@gmail.com>
2026-01-06 09:21:40 +0500

    Quiz App Created

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+
+  const questions = [
+    {
+      question: "HTML ka full form kya hai?",
+      options: ["Hyper Text Markup Language", "High Text Machine Language", "Hyper Tool Markup Language"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      question: "React kis company ne banaya?",
+      options: ["Google", "Facebook", "Microsoft"],
+      answer: "Facebook"
+    },
+    {
+      question: "JavaScript kis type ki language hai?",
+      options: ["Programming", "Markup", "Styling"],
+      answer: "Programming"
+    }
+  ];
+
+  const handleClick = (option) => {
+    if (option === questions[index].answer) {
+      setScore(score + 1);
+    }
+
+    if (index + 1 < questions.length) {
+      setIndex(index + 1);
+    } else {
+      setShowResult(true);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>Quiz App</h1>
+
+      {showResult ? (
+        <h2>Score: {score} / {questions.length}</h2>
+      ) : (
+        <>
+          <h3>{questions[index].question}</h3>
+
+          {questions[index].options.map((opt, i) => (
+            <button key={i} onClick={() => handleClick(opt)}>
+              {opt}
+            </button>
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
